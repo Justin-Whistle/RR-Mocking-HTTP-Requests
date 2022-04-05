5325b1b0f3dcd100fcd5d548465f626e64d4075c
Justin-Whistle <92463485+Justin-Whistle@users.noreply.github.com>
2022-04-05 16:28:25 -0500

    not working

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,16 @@
+import App from './App.js';
+import { render, screen, waitFor } from "@testing-library/react";
 
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('name')
+  })
+
+  beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
 
 
